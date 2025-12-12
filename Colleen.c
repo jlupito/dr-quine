4bb22adfbdad63fdb006afab0daffbfb4da258b6
Jean-loup ARTHAUD <jarthaud@f4r11s15.clusters.42paris.fr>
2025-12-12 10:17:10 +0100

    fixed tabs

--- Colleen.c
+++ Colleen.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /*
-    Outside comment
+	Outside comment
 */
 
-void ft(char *str){printf(str,10,10,10,9,10,10,10,10,10,10,10,9,10,10,34,str,34,10,10,10,10,9);}
+void ft(char *str){printf(str,10,10,10,9,10,10,10,10,10,10,10,9,10,10,34,str,34,10,10,10,10);}
 
 int main(){
 /*
-    Inside comment
+	Inside comment
 */
-char *str="#include <stdio.h>%c%c/*%c%cOutside comment%c*/%c%cvoid ft(char *str){printf(str,10,10,10,9,10,10,10,10,10,10,10,9,10,10,34,str,34,10,10,10,10,9);}%c%cint main(){%c/*%c%cInside comment%c*/%cchar *str=%c%s%c;%cft(str);%creturn (0);%c}%c";
+char *str="#include <stdio.h>%c%c/*%c%cOutside comment%c*/%c%cvoid ft(char *str){printf(str,10,10,10,9,10,10,10,10,10,10,10,9,10,10,34,str,34,10,10,10,10);}%c%cint main(){%c/*%c%cInside comment%c*/%cchar *str=%c%s%c;%cft(str);%creturn (0);%c}%c";
 ft(str);
 return (0);
 }
--- Grace.c
+++ Grace.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-#define STR "#include <stdio.h>%c%c#define STR %c%s%c%c#define FT_PRINT(f) fprintf(f,STR,10,10,34,STR,34,10,10,34,34,34,34,10,10,10,9,10,10,10,10)%c#define FT_MAIN() int main() { FILE *f; f=fopen(%cGrace_kid.c%c,%cw%c); if (f == NULL) return (1); FT_PRINT(f); return 0; }%c%c/*%c%cThe comment%c*/%c%cFT_MAIN%c"
-#define FT_PRINT(f) fprintf(f,STR,10,10,34,STR,34,10,10,34,34,34,34,10,10,10,9,10,10,10,10)
+#define STR "#include <stdio.h>%c%c#define STR %c%s%c%c#define FT_PRINT(f) fprintf(f,STR,10,10,34,STR,34,10,10,34,34,10,10,10,9,10,10,10,34,34,10)%c#define FT_MAIN(x) int main() { FILE *f; f=fopen(x,%cw%c); if (f == NULL) return (1); FT_PRINT(f); return 0; }%c%c/*%c%cThe comment%c*/%c%cFT_MAIN(%cGrace_kid.c%c)%c"
+#define FT_PRINT(f) fprintf(f,STR,10,10,34,STR,34,10,10,34,34,10,10,10,9,10,10,10,34,34,10)
 #define FT_MAIN(x) int main() { FILE *f; f=fopen(x,"w"); if (f == NULL) return (1); FT_PRINT(f); return 0; }
 
 /*
-    The comment
+	The comment
 */
 
 FT_MAIN("Grace_kid.c")
